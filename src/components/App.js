c06150dccfcfbab4ca16741bcfec6ed270a71504
Anand101094 <49559852+Anand101094@users.noreply.github.com>
2020-05-25 12:12:07 +0530

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const App = () => {
+    return(
+        <div className="main-app">
+            <h1>Data Stucture</h1>
+        </div>
+    )
+}
+
+export default App
